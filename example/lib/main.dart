820d0d6e34f5f402a3128f2c71591c382e2bde26
BitcoinZavior <BitcoinZavior@GMail.Com>
2024-01-30 08:59:00 -0500

    Tokio crate added

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'package:payjoin_flutter/payjoin.dart';
 
-import 'package:payjoin/payjoin.dart' as payjoin;
-
-void main() {
+void main() async {
+  await Payjoin.initialize();
   runApp(const MyApp());
 }
 
@@ -20,18 +19,39 @@ class _MyAppState extends State<MyApp> {
     super.initState();
   }
 
+  Future<Uri> buildPjUri(double amount, String address, String pj) async {
+    try {
+      final pjUri = "${address}amount=${amount / 100000000.0}&pj=$pj";
+      return await Uri.fromStr(uri: pjUri);
+    } catch (e) {
+      debugPrint(e.toString());
+      rethrow;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Payjoin'),
+          title: const Text('Payjoin Example App'),
         ),
         body: SingleChildScrollView(
           child: Container(
             padding: const EdgeInsets.all(10),
             child: Column(
-              children: [],
+              children: [
+                TextButton(
+                    onPressed: () async {
+                      final uri = await buildPjUri(
+                          10000000,
+                          "bcrt1qaux734vuhykww9632v8cmdnk7z2mw5lsf74v6k",
+                          "https://example.com");
+                      print(await uri.address());
+                    },
+                    child: const Text("Build PjUri"))
+              ],
             ),
           ),
         ),
