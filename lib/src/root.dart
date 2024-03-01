88003b9b181f9ede0255316b27566186d1ec8efa
BitcoinZavior <BitcoinZavior@GMail.Com>
2024-02-29 20:09:00 -0500

    fix(initialize()): avoid re-initializing payjoin

--- lib/src/root.dart
+++ lib/src/root.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/cupertino.dart';
+import 'package:payjoin_flutter/payjoin.dart';
 
 import 'generated/frb_generated.dart';
+
+class UriW extends Uri {
+  UriW({required super.field0});
+}
 
 class Payjoin {
   static Future<void> initialize() async {
     try {
-      await PayjoinCore.init();
+      if (!PayjoinCore.instance.initialized) {
+        await PayjoinCore.init();
+      }
     } catch (e) {
       debugPrint(e.toString());
       throw Exception(
