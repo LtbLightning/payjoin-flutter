2dc6eab572c02aa04987976ab93edfcdfa2062a5
DanGould <d@ngould.dev>
2024-12-04 16:24:37 -0500

    Add payjoin-flutter-demo as example
    This was cloned directly from:
    <https://github.com/LtbLightning/payjoin-flutter-demo/pull/12/commits/f67399d345100c22d1dd36f2ef701bdd5f512931>
    
    payjoin-flutter-demo is the most complete reference implementation of
    payjoin-flutter in an app. Let's use that here and manually run
    smoke tests to ensure the operation.

---
+++ example/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bdk_flutter_demo/screens/home.dart';
+import 'package:bdk_flutter_demo/styles/theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Payjoin Flutter Demo',
+      theme: theme(),
+      home: const Home(),
+    );
+  }
+}
---
+++ example/lib/widgets/widgets.dart
@@ -0,0 +1,258 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SubmitButton extends StatelessWidget {
+  final String text;
+  final VoidCallback callback;
+
+  const SubmitButton({Key? key, required this.text, required this.callback})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: callback,
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 5),
+        decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: BorderRadius.circular(5)),
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        width: double.infinity,
+        child: Center(
+          child: Text(text, style: Theme.of(context).textTheme.labelLarge),
+        ),
+      ),
+    );
+  }
+}
+
+class TextFieldContainer extends StatelessWidget {
+  final Widget child;
+
+  const TextFieldContainer({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 2.5),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(width: 2, color: Theme.of(context).primaryColor)),
+      child: child,
+    );
+  }
+}
+
+class StyledContainer extends StatelessWidget {
+  final Widget child;
+
+  const StyledContainer({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 5),
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            width: 2,
+            color: Theme.of(context).primaryColor,
+          )),
+      child: child,
+    );
+  }
+}
+
+class BalanceContainer extends StatelessWidget {
+  final String text;
+
+  const BalanceContainer({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.only(bottom: 5),
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+        width: double.infinity,
+        child: StyledContainer(
+          child: SelectableText.rich(
+            TextSpan(
+              children: <TextSpan>[
+                TextSpan(
+                    text: "Balance: ",
+                    style: Theme.of(context).textTheme.displayMedium),
+                TextSpan(
+                    text: text, style: Theme.of(context).textTheme.bodyLarge),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class ResponseContainer extends StatelessWidget {
+  final String text;
+
+  const ResponseContainer({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.only(bottom: 5),
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+        width: double.infinity,
+        child: StyledContainer(
+          child: SelectableText.rich(
+            TextSpan(
+              children: <TextSpan>[
+                TextSpan(
+                    text: "Response: ",
+                    style: Theme.of(context).textTheme.displayMedium),
+                TextSpan(
+                    text: text, style: Theme.of(context).textTheme.bodyLarge),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+AppBar buildAppBar(BuildContext context) {
+  return AppBar(
+    leadingWidth: 80,
+    actions: [
+      Padding(
+        padding: const EdgeInsets.only(right: 20, bottom: 10, top: 10),
+        child: Image.asset("assets/pdk_logo.png"),
+      )
+    ],
+    leading: Icon(
+      CupertinoIcons.bitcoin_circle_fill,
+      color: Theme.of(context).secondaryHeaderColor,
+      size: 40,
+    ),
+    title:
+        Text("Payjoin Demo", style: Theme.of(context).textTheme.displayLarge),
+  );
+}
+
+class SelectFeeRange extends StatelessWidget {
+  const SelectFeeRange({super.key, this.feeRange});
+  final FeeRangeEnum? feeRange;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+          child: Text(
+            'Choose a Fee Range',
+          ),
+        ),
+        FeesRangeOptions(
+          feeRange: feeRange,
+        ),
+      ],
+    );
+  }
+}
+
+enum FeeRangeEnum {
+  high('High', 2000),
+  medium('Medium', 1000),
+  low('Low', 500);
+
+  final String name;
+  final int feeValue;
+
+  const FeeRangeEnum(this.name, this.feeValue);
+
+  @override
+  String toString() => '$feeValue sats';
+}
+
+class FeesRangeOptions extends StatefulWidget {
+  FeesRangeOptions({super.key, this.feeRange = FeeRangeEnum.high});
+  FeeRangeEnum? feeRange;
+  @override
+  State<FeesRangeOptions> createState() => _FeesRangeOptionsState();
+}
+
+class _FeesRangeOptionsState extends State<FeesRangeOptions> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        RadioListTile<FeeRangeEnum>(
+          title: ListTile(
+            title: Text(FeeRangeEnum.high.name),
+            subtitle: const Text('10 - 30 minutes'),
+            trailing: Text(FeeRangeEnum.high.toString()),
+          ),
+          value: FeeRangeEnum.high,
+          groupValue: widget.feeRange,
+          onChanged: onChangeFeeRange,
+        ),
+        RadioListTile<FeeRangeEnum>(
+          title: ListTile(
+            title: Text(FeeRangeEnum.medium.name),
+            subtitle: const Text('30 - 60 minutes'),
+            trailing: Text(FeeRangeEnum.medium.toString()),
+          ),
+          value: FeeRangeEnum.medium,
+          groupValue: widget.feeRange,
+          onChanged: onChangeFeeRange,
+        ),
+        RadioListTile<FeeRangeEnum>(
+          title: ListTile(
+            title: Text(FeeRangeEnum.low.name),
+            subtitle: const Text('2 - 12 hours'),
+            trailing: Text(FeeRangeEnum.low.toString()),
+          ),
+          value: FeeRangeEnum.low,
+          groupValue: widget.feeRange,
+          onChanged: onChangeFeeRange,
+        ),
+      ],
+    );
+  }
+
+  onChangeFeeRange(FeeRangeEnum? value) {
+    setState(() {
+      widget.feeRange = value;
+      Navigator.pop(context, widget.feeRange);
+    });
+  }
+}
+
+class CustomSwitchTile extends StatelessWidget {
+  const CustomSwitchTile(
+      {super.key,
+      required this.title,
+      required this.onChanged,
+      required this.value});
+  final String title;
+  final bool value;
+  final Function(bool) onChanged;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          title,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: Switch(
+            value: value,
+            onChanged: onChanged,
+          ),
+        ),
+      ],
+    );
+  }
+}
